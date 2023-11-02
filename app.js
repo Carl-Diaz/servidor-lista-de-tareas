fcaf0797fb01c1c263ed21e9fa068ebc0fba3682
alberto diaz <albersan338@gmail.com>
2023-11-01 22:45:27 -0500

    cambio de nombre de archivo server.js a app.js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const app = express();
 const port = 3000;
 
 const tasks = [
-  { id: 1, description: 'Hacer la compra', completed: false },
-  { id: 2, description: 'Lavar la ropa', completed: true },
-  { id: 3, description: 'Estudiar para el examen', completed: false }
+  { id: 1, description: "Hacer la compra", completed: false },
+  { id: 2, description: "Lavar la ropa", completed: true },
+  { id: 3, description: "Estudiar para el examen", completed: false },
 ];
-
 
 app.get("/", (req, res) => {
   res.json(tasks);
