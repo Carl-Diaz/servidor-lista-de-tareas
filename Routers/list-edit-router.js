795753f2be4b1f1e3eb58d886da2d86fb29e0d99
alberto diaz <albersan338@gmail.com>
2023-11-01 22:56:52 -0500

    actualizacion de los routers y del app.js

--- Routers/list-edit-router.js
+++ Routers/list-edit-router.js
@@ -1,11 +1,6 @@
+const tasks = require("../script");
 const express = require("express");
 const router = express.Router();
-
-let tasks = [
-  { id: 1, description: "Hacer la compra", completed: false },
-  { id: 2, description: "Lavar la ropa", completed: true },
-  { id: 3, description: "Estudiar para el examen", completed: false },
-];
 
 // Ruta para crear una tarea (POST)
 router.post("/", (req, res) => {
@@ -13,8 +8,7 @@ router.post("/", (req, res) => {
   // Generar un nuevo ID para la tarea
   newTask.id = tasks.length + 1;
   tasks.push(newTask);
-  // Mostrar la lista de tareas actualizada después de crear una nueva tarea
-  res.json({ message: "Tarea creada con éxito" });
+  res.json({ message: "Tarea creada con éxito", task: newTask });
 });
 
 // Ruta para eliminar una tarea por ID (DELETE)
@@ -26,7 +20,6 @@ router.delete("/:id", (req, res) => {
     res.status(404).json({ message: "Tarea no encontrada" });
   } else {
     tasks.splice(taskIndex, 1);
-    // Mostrar la lista de tareas actualizada después de eliminar una tarea
     res.json({ message: "Tarea eliminada con éxito" });
   }
 });
@@ -41,7 +34,6 @@ router.put("/:id", (req, res) => {
   } else {
     const updatedTask = req.body;
     tasks[taskIndex] = { ...tasks[taskIndex], ...updatedTask };
-    // Mostrar la lista de tareas actualizada después de actualizar una tarea
     res.json({ message: "Tarea actualizada con éxito" });
   }
 });
--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-const tasksRouter = require("./Routers/list-view-router");
-const listEditRouter = require("./Routers/list-edit-router");
+const port = 8080;
 
 app.use(express.json());
 
-let tasks = [
-  // Define 'tasks' and initialize it with initial values
-  { id: 1, description: "Hacer la compra", completed: false },
-  { id: 2, description: "Lavar la ropa", completed: true },
-  { id: 3, description: "Estudiar para el examen", completed: false },
-];
+const listViewRouter = require("./Routers/list-view-router");
+const listEditRouter = require("./Routers/list-edit-router");
 
-// Use the routers with the tasks
-app.use(tasksRouter(tasks));
-app.use("/list-edit", listEditRouter(tasks));
-
-app.get("/", (req, res) => {
-  res.json(tasks);
-});
+app.use("/list-view", listViewRouter);
+app.use("/list-edit", listEditRouter);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto: ${port}`);
